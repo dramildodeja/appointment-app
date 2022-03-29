28cafd35ed3dbc0bc9fa9b2a375f3f2adaa38d12
Dramil Dodeja <dramil.dodeja@libertymutual.com>
2022-03-28 21:44:54 -0700

    Initial Commit

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-	$servername = "localhost";
-	$username = "user";
-	$password = "pass";
-	$dbname = "database";
+	$servername = "sql5.freemysqlhosting.net";
+	$username = "sql5482244";
+	$password = "F4LEMkbJMI";
+	$dbname = "sql5482244";
 	$tablename = "bookingcalendar";
 	$months = array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
 	$headings = array('Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday');
