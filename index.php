3a8582ee8ea207a08dbf8507f7e89fde6bbacaf3
Dramil Dodeja <dramil.dodeja@libertymutual.com>
2022-03-29 17:59:02 -0700

    Updates

--- index.php
+++ index.php
@@ -69,6 +69,90 @@ td.calendar-day, td.calendar-day-np {
 <script src="../js/jquery-1.10.2.js"></script>
 <script src="../js/jquery-ui.js"></script>
 <!--<script src="../lang/datepicker-fi.js"></script>-->
+
+<?php
+if (isset($_GET['viewall'])) {
+    function draw_calendar($month,$year){
+        	include 'config.php';
+        	$conn = mysqli_connect($servername, $username, $password,  $dbname);
+        	if (!$conn) {
+            	die("Connection failed: " . mysqli_connect_error());
+        	}
+        	$calendar = '<table cellpadding="0" cellspacing="0" class="calendar">';
+        	$calendar.= '<tr class="calendar-row"><td class="calendar-day-head">'.implode('</td><td class="calendar-day-head">',$headings).'</td></tr>';
+        	$running_day = date('w',mktime(0,0,0,$month,1,$year));
+        	$days_in_month = date('t',mktime(0,0,0,$month,1,$year));
+        	$days_in_this_week = 1;
+        	$day_counter = 0;
+        	$dates_array = array();
+        	$calendar.= '<tr class="calendar-row">';
+        	for($x = 0; $x < $running_day; $x++):
+        		$calendar.= '<td class="calendar-day-np"> </td>';
+        		$days_in_this_week++;
+        	endfor;
+        	for($list_day = 1; $list_day <= $days_in_month; $list_day++):
+        		$calendar.= '<td class="calendar-day">';
+        			$calendar.= '<div class="day-number">'.$list_day.'</div>';
+        			$calendar.= str_repeat('<p> </p>',2);
+        			$current_epoch = mktime(0,0,0,$month,$list_day,$year);
+        			$sql = "SELECT * FROM $tablename WHERE $current_epoch BETWEEN start_day AND end_day";
+        			$result = mysqli_query($conn, $sql);
+            		if (mysqli_num_rows($result) > 0) {
+            			while($row = mysqli_fetch_assoc($result)) {
+        					if($row["canceled"] == 1) $calendar .= "<font color=\"grey\"><s>";
+            				$calendar .= "<b>" . $row["item"] . "</b><br>ID: " . $row["id"] . "<br>" . $row["name"] . "<br>" . $row["phone"] . "<br>" . $row["email"] . "<br>";
+            				if($current_epoch == $row["start_day"] AND $current_epoch != $row["end_day"]) {
+            					$calendar .= "Booking starts: " . sprintf("%02d:%02d", $row["start_time"]/60/60, ($row["start_time"]%(60*60)/60)) . "<br><hr><br>";
+            				}
+            				if($current_epoch == $row["start_day"] AND $current_epoch == $row["end_day"]) {
+            					$calendar .= "Booking starts: " . sprintf("%02d:%02d", $row["start_time"]/60/60, ($row["start_time"]%(60*60)/60)) . "<br>";
+            				}
+            				if($current_epoch == $row["end_day"]) {
+            					$calendar .= "Booking ends: " . sprintf("%02d:%02d", $row["end_time"]/60/60, ($row["end_time"]%(60*60)/60)) . "<br><hr><br>";
+            				}
+            				if($current_epoch != $row["start_day"] AND $current_epoch != $row["end_day"]) {
+        	    				$calendar .= "Booking: 24h<br><hr><br>";
+        	    			}
+        					if($row["canceled"] == 1) $calendar .= "</s></font>";
+            			}
+        			} else {
+            			$calendar .= "No bookings";
+        			}
+
+        		$calendar.= '</td>';
+        		if($running_day == 6):
+        			$calendar.= '</tr>';
+        			if(($day_counter+1) != $days_in_month):
+        				$calendar.= '<tr class="calendar-row">';
+        			endif;
+        			$running_day = -1;
+        			$days_in_this_week = 0;
+        		endif;
+        		$days_in_this_week++; $running_day++; $day_counter++;
+        	endfor;
+        	if($days_in_this_week < 8):
+        		for($x = 1; $x <= (8 - $days_in_this_week); $x++):
+        			$calendar.= '<td class="calendar-day-np"> </td>';
+        		endfor;
+        	endif;
+        	$calendar.= '</tr>';
+        	$calendar.= '</table>';
+        	mysqli_close($conn);
+        	return $calendar;
+        }
+        include 'config.php';
+        echo '<br><h3>View Appointments</h3>';
+        $d = new DateTime(date("Y-m-d"));
+        echo '<h3>' . $months[$d->format('n')-1] . ' ' . $d->format('Y') . '</h3>';
+        echo draw_calendar($d->format('m'),$d->format('Y'));
+        $d->modify( 'first day of next month' );
+        echo '<h3>' . $months[$d->format('n')-1] . ' ' . $d->format('Y') . '</h3>';
+        echo draw_calendar($d->format('m'),$d->format('Y'));
+        $d->modify( 'first day of next month' );
+        echo '<h3>' . $months[$d->format('n')-1] . ' ' . $d->format('Y') . '</h3>';
+        echo draw_calendar($d->format('m'),$d->format('Y'));
+} else {
+?>
 <script>
     $(function() {
 	<!--$.datepicker.setDefaults($.datepicker.regional['fi']);-->
@@ -245,86 +329,6 @@ td.calendar-day, td.calendar-day-np {
    </tr>
 </table>
 <?php
-if (isset($_GET['list'])) {
-    function draw_calendar($month,$year){
-    	include 'config.php';
-    	$conn = mysqli_connect($servername, $username, $password,  $dbname);
-    	if (!$conn) {
-        	die("Connection failed: " . mysqli_connect_error());
-    	}
-    	$calendar = '<table cellpadding="0" cellspacing="0" class="calendar">';
-    	$calendar.= '<tr class="calendar-row"><td class="calendar-day-head">'.implode('</td><td class="calendar-day-head">',$headings).'</td></tr>';
-    	$running_day = date('w',mktime(0,0,0,$month,1,$year));
-    	$days_in_month = date('t',mktime(0,0,0,$month,1,$year));
-    	$days_in_this_week = 1;
-    	$day_counter = 0;
-    	$dates_array = array();
-    	$calendar.= '<tr class="calendar-row">';
-    	for($x = 0; $x < $running_day; $x++):
-    		$calendar.= '<td class="calendar-day-np"> </td>';
-    		$days_in_this_week++;
-    	endfor;
-    	for($list_day = 1; $list_day <= $days_in_month; $list_day++):
-    		$calendar.= '<td class="calendar-day">';
-    			$calendar.= '<div class="day-number">'.$list_day.'</div>';
-    			$calendar.= str_repeat('<p> </p>',2);
-    			$current_epoch = mktime(0,0,0,$month,$list_day,$year);
-    			$sql = "SELECT * FROM $tablename WHERE $current_epoch BETWEEN start_day AND end_day";
-    			$result = mysqli_query($conn, $sql);
-        		if (mysqli_num_rows($result) > 0) {
-        			while($row = mysqli_fetch_assoc($result)) {
-    					if($row["canceled"] == 1) $calendar .= "<font color=\"grey\"><s>";
-        				$calendar .= "<b>" . $row["item"] . "</b><br>ID: " . $row["id"] . "<br>" . $row["name"] . "<br>" . $row["phone"] . "<br>" . $row["email"] . "<br>";
-        				if($current_epoch == $row["start_day"] AND $current_epoch != $row["end_day"]) {
-        					$calendar .= "Booking starts: " . sprintf("%02d:%02d", $row["start_time"]/60/60, ($row["start_time"]%(60*60)/60)) . "<br><hr><br>";
-        				}
-        				if($current_epoch == $row["start_day"] AND $current_epoch == $row["end_day"]) {
-        					$calendar .= "Booking starts: " . sprintf("%02d:%02d", $row["start_time"]/60/60, ($row["start_time"]%(60*60)/60)) . "<br>";
-        				}
-        				if($current_epoch == $row["end_day"]) {
-        					$calendar .= "Booking ends: " . sprintf("%02d:%02d", $row["end_time"]/60/60, ($row["end_time"]%(60*60)/60)) . "<br><hr><br>";
-        				}
-        				if($current_epoch != $row["start_day"] AND $current_epoch != $row["end_day"]) {
-    	    				$calendar .= "Booking: 24h<br><hr><br>";
-    	    			}
-    					if($row["canceled"] == 1) $calendar .= "</s></font>";
-        			}
-    			} else {
-        			$calendar .= "No bookings";
-    			}
-
-    		$calendar.= '</td>';
-    		if($running_day == 6):
-    			$calendar.= '</tr>';
-    			if(($day_counter+1) != $days_in_month):
-    				$calendar.= '<tr class="calendar-row">';
-    			endif;
-    			$running_day = -1;
-    			$days_in_this_week = 0;
-    		endif;
-    		$days_in_this_week++; $running_day++; $day_counter++;
-    	endfor;
-    	if($days_in_this_week < 8):
-    		for($x = 1; $x <= (8 - $days_in_this_week); $x++):
-    			$calendar.= '<td class="calendar-day-np"> </td>';
-    		endfor;
-    	endif;
-    	$calendar.= '</tr>';
-    	$calendar.= '</table>';
-    	mysqli_close($conn);
-    	return $calendar;
-    }
-    include 'config.php';
-    echo '<br><h3>View Appointments</h3>';
-    $d = new DateTime(date("Y-m-d"));
-    echo '<h3>' . $months[$d->format('n')-1] . ' ' . $d->format('Y') . '</h3>';
-    echo draw_calendar($d->format('m'),$d->format('Y'));
-    $d->modify( 'first day of next month' );
-    echo '<h3>' . $months[$d->format('n')-1] . ' ' . $d->format('Y') . '</h3>';
-    echo draw_calendar($d->format('m'),$d->format('Y'));
-    $d->modify( 'first day of next month' );
-    echo '<h3>' . $months[$d->format('n')-1] . ' ' . $d->format('Y') . '</h3>';
-    echo draw_calendar($d->format('m'),$d->format('Y'));
 }
 ?>
 </body>
